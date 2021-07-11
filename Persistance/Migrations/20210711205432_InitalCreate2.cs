13bc979c15aacc325dab2dcd1f44deb35f9bd777
Brian Francis <13bfrancis@gmail.com>
2021-07-11 18:43:44 -0400

    skeleton project

---
+++ Persistance/Migrations/20210711205432_InitalCreate2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class InitalCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "date",
+                table: "Activities",
+                newName: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Activities",
+                newName: "date");
+        }
+    }
+}
